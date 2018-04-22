409fecfad07d4108cfc3ca7debe27d921a92b638
atlas1988 <atlas_li@126.com>
2018-04-23 05:49:54 +0800

    [shell] setup android compile env

---
+++ shell/android_env_setup/install.sh
@@ -0,0 +1,31 @@
+sudo apt-get -y install git
+sudo apt-get -y install gnupg
+sudo apt-get -y install flex
+sudo apt-get -y install gperf
+sudo apt-get -y install build-essential
+sudo apt-get -y install curl
+sudo apt-get -y install zlib1g-dev
+sudo apt-get -y install libc6-dev
+sudo apt-get -y install lib32ncurses5-dev
+sudo apt-get -y install ia32-libs
+sudo apt-get -y install x11proto-core-dev
+sudo apt-get -y install libx11-dev
+##lib32readline-gplv2-dev
+sudo apt-get -y install lib32readline5-dev
+sudo apt-get -y install lib32z-dev
+sudo apt-get -y install libgl1-mesa-dev
+sudo apt-get -y install g++-multilib
+sudo apt-get -y install mingw32
+sudo apt-get -y install tofrodos
+sudo apt-get -y install bison
+sudo apt-get -y install zip
+sudo apt-get -y install libxml2-utils
+sudo apt-get -y install git-core gitk git-gui ssh vim
+sudo apt-get -y install cutecom
+sudo apt-get -y install openjdk-7-jdk
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get -y install openjdk-8-jdk
+sudo apt-get -y install dos2unix
+###audit2allow selinux
+sudo apt-get -y install policycoreutils

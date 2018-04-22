65b7393ed4b350e5b4145168698181a7eea672a6
atlas1988 <atlas_li@126.com>
2018-04-23 05:46:55 +0800

    [shell] setup yuanchenglianjie(mstsc) for windows

---
+++ shell/android_env_setup/yuancheng-mstsc.sh
@@ -0,0 +1,8 @@
+sudo apt-get -y install xrdp
+sudo apt-get -y install vnc4server
+sudo apt-get -y install xubuntu-desktop
+###echo "xfce4-session" >~/.xsession
+echo "gnome-session --session=ubuntu-2d">~/.xsession
+sudo service xrdp restart
+
+

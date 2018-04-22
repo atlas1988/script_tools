6a669920544faeb5501abaececb50d266e541f81
atlas1988 <atlas_li@126.com>
2018-04-23 05:52:37 +0800

    [shell] setup cts/vts env

---
+++ shell/android_env_setup/cts-vts/cts/cts-env.sh
@@ -0,0 +1,11 @@
+#########################################################################
+# File Name: cts-env.sh
+# Author: atlas
+# mail: atlas_li@126.com
+# Created Time: Sun 22 Apr 2018 04:15:38 PM CST
+#########################################################################
+#!/bin/bash
+
+#unzip android-cts-8.1_r3-linux_x86-arm.zip -d ./
+chmod 777 ./android-cts/tools/cts-tradefed
+
---
+++ shell/android_env_setup/cts-vts/vts/vts-env.sh
@@ -0,0 +1,15 @@
+#########################################################################
+# File Name: vts-env.sh
+# Author: atlas
+# mail: atlas_li@126.com
+# Created Time: Sun 22 Apr 2018 05:29:06 PM CST
+#########################################################################
+#!/bin/bash
+##install Python develpment kit
+sudo apt-get -y install python-dev
+##install Protocol buff tools(for python)
+sudo apt-get -y install python-protobuf
+sudo apt-get -y install protobuf-compiler
+##install python virtual environment-related tools
+sudo apt-get -y install python-virtualenv
+sudo apt-get -y install python-pip
